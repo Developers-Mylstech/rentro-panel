a74312e1f170f815c618a06ce8f3b7222a998cc9
xyaistic <ziyamalik880@gmail.com>
2025-02-19 12:19:16 +0530

    theme build

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function Header() {
   return (
     <div>
-      Header
+      <h1 className='heading' >Heading</h1>
+      <p className='text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium doloremque illo iste error esse ut rerum, nesciunt fugiat magnam temporibus. Similique quasi quaerat corrupti ipsam animi veritatis, repellat odio assumenda exercitationem, qui rem laudantium numquam necessitatibus architecto velit explicabo praesentium!</p>
     </div>
   )
 }
