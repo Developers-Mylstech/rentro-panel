bf9f4d675e30536455e05e17881d5b62472cdbd5
xyaistic <ziyamalik880@gmail.com>
2025-04-18 16:37:54 +0530

    Changes

--- src/fetchServices/ProductServices.js
+++ src/fetchServices/ProductServices.js
@@ -1,6 +1,8 @@
+// import axiosInstance from "../utils/axiosInstance"
+
 // export default getAllProduct = () => {
 // try {
-//     const res =
+//     const res = axiosInstance.get('/api/')
 // } catch (error) {
 //     console.log(error)
 // }
