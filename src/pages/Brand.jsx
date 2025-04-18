cd27f086be9367698ed7b76fa6eee81c36a2e8e6
xyaistic <ziyamalik880@gmail.com>
2025-04-18 18:33:48 +0530

    Changes

--- src/pages/Brand.jsx
+++ src/pages/Brand.jsx
@@ -6,20 +6,10 @@ import useBrandStore from "../Context/BrandContext";
 
 export default function Brand() {
   const navigate = useNavigate();
-  const {brands,getAllBrands} = useBrandStore ()
+  const { brands, getAllBrands, removeBrand } = useBrandStore()
   useEffect(() => {
     getAllBrands()
   }, [])
-  // const brands = [
-  //   { id: 1, name: "RENT RO", image: "https://via.placeholder.com/80" },
-  //   { id: 2, name: "Kent RO", image: "https://via.placeholder.com/80" },
-  //   { id: 3, name: "Aqua Pro", image: "https://via.placeholder.com/80" },
-  //   { id: 4, name: "Waterlogic", image: "https://via.placeholder.com/80" },
-  //   { id: 5, name: "Culligan", image: "https://via.placeholder.com/80" },
-  //   { id: 6, name: "Super General", image: "https://via.placeholder.com/80" },
-  //   { id: 7, name: "Aquaguard", image: "https://via.placeholder.com/80" },
-  // ];
-
   return (
     <div className="h-screen dark:bg-gray-900">
       <BrandList brands={brands} />
