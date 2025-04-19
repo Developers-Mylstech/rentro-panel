9be018cf3aa091cc8272c22c68410bc4bc03145d
xyaistic <ziyamalik880@gmail.com>
2025-04-19 20:34:52 +0530

    Changes

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig, loadEnv } from "vite"
+import { VITE_APP_KEY } from "./src/env";
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -14,7 +15,7 @@ export default defineConfig(({ mode }) => {
     server: {
       proxy: {
         '/api': {
-          target: env.VITE_APP_KEY,
+          target: VITE_APP_KEY,
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/api/, ''),
         },
